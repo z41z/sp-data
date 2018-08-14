47611df2da1adb7080e492ad7cd9274a46fac348
r3qu3stt1me0ut <r3qu3stt1me0ut@gmail.com>
2018-08-14 17:11:30 +0800

    update date.goto

--- date/goto.js
+++ date/goto.js
@@ -1,3 +1,3 @@
-module.exports = function (year = 0, month = 0, day = 0) {
-  return new Date(year, month - 1, day);
+module.exports = function (year = 0, month = 0, day = 0, hour = 0, minute = 0, seconds = 0) {
+  return new Date(year, month - 1, day, hour, minute, seconds);
 }
