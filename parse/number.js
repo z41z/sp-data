2d5e6b4e7c04a15610d521196d926399f387b2fc
r3qu3stt1me0ut <r3qu3stt1me0ut@gmail.com>
2018-08-15 14:53:06 +0800

    fixed parse to number

--- parse/number.js
+++ parse/number.js
@@ -1,7 +1,7 @@
 module.exports = function (num = 0, fixedLength = 0) {
   if (num && !isNaN(+num)) {
-    return fixedLength ? +((+num).toFixed(fixedLength)) : +num; //toFixed return String
+    return Boolean(fixedLength) ? (+num).toFixed(fixedLength) : +num; //toFixed return String
   } else {
-    return +((0).toFixed(fixedLength));
+    return Boolean(fixedLength) ? (0).toFixed(fixedLength) : 0;
   }
 }
